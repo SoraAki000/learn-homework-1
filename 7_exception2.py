24f88b68bef7e30bf6993c6da8585f4170c4c464
Sorakaze Aki <soraaki000@gmail.com>
2024-05-22 12:24:26 +0500

    Задание 7_exception2

--- 7_exception2.py
+++ 7_exception2.py
@@ -10,15 +10,25 @@
 * Первые два нужно приводить к вещественному числу при помощи float(),
   а третий - к целому при помощи int() и перехватывать исключения
   ValueError и TypeError, если приведение типов не сработало.
-    
+
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+
+def discounted(price, discount, max_discount=20):
+    try:
+        price, discount, max_discount = abs(float(price)), abs(float(discount)), abs(int(max_discount))
+    except ValueError:
+        return "Введены неверные значения"
+    except TypeError:
+        return "Приведение типов не сработало"
+    if max_discount >= 100:
+        return "Слишком большая максимальная скидка"
+    if discount > float(max_discount):
+        return price
+    else:
+        return price - (price * discount / 100)
+
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
